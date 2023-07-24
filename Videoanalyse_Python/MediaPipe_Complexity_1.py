2ea904151857bfc2860a9e277f0bd390232efd4f
JulianSchaefer3 <56607415+JulianSchaefer3@users.noreply.github.com>
2023-07-25 01:36:58 +0200

    Add files via upload

---
+++ Videoanalyse_Python/MediaPipe_Complexity_1.py
@@ -0,0 +1,142 @@
+import mediapipe as mp
+import cv2
+import numpy as np
+import uuid
+import os
+import sys
+
+from matplotlib import pyplot as plt
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+filename = "F:/Studium/MA/Videos/03_26_Marvin_Sortiert/Brust/seitlich"
+#filename = sys.argv[1]
+
+cap = cv2.VideoCapture(0) #Kamera
+#cap = cv2.VideoCapture(filename + ".MP4") #Video
+
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+out_filename = filename + "_full.MP4"
+out = cv2.VideoWriter(out_filename, cv2.VideoWriter_fourcc(
+    'm', 'p', '4', 'v'), 10, (1280, 720))
+
+
+def getSwimPhase():
+    None
+
+def calculate_angle(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle >180.0:
+        angle = 360-angle
+        
+    return angle
+
+counter = 0
+stage = None
+## Setup mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        if ret == True:
+            b = cv2.resize(frame,(1280,720),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+        
+        # Recolor image to RGB
+        image = cv2.cvtColor(b, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+      
+        # Make detection
+        results = pose.process(image)
+    
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        # Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+            
+            # Get coordinates
+            shoulder_left = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow_left = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            wrist_left = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            
+            shoulder_right = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            elbow_right = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+            wrist_right = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+            
+            # Calculate angle
+            angle_left = calculate_angle(shoulder_left, elbow_left, wrist_left)
+            angle_right = calculate_angle(shoulder_right,elbow_right,wrist_right)
+            
+            # Visualize angle
+            cv2.putText(image, str(angle_left), 
+                           tuple(np.multiply(elbow_left, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+            
+            cv2.putText(image, str(angle_right), 
+                           tuple(np.multiply(elbow_right, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+            
+            # Curl counter logic
+            if angle_left > 160 and angle_right > 160:
+                stage = "down"
+            if angle_left < 30 and angle_right < 30 and stage =='down':
+                stage="up"
+                counter +=1
+                print(counter, " " ,angle)
+                       
+        except:
+            pass
+        
+        # Render curl counter
+        # Setup status box
+        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+        
+        # Rep data
+        cv2.putText(image, 'Phasenbild', (15,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(counter), 
+                    (10,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+        
+        # Stage data
+        cv2.putText(image, 'Phasenbild', (65,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, stage, 
+                    (60,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+        
+        
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), #Landmarks: blue
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)  #Connections: purple
+                                 )               
+        
+        cv2.imshow('Mediapipe Feed', image)
+        #out.write(image)
+
+        if ret == False or cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+
+pose.close()
+cap.release()
+out.release()
+    #cap.release()
+    #cv2.destroyAllWindows()
